ac769931991d4f31d833edf2715b8bfdbb03d527
unknown <pwhynotfilms@gmail.com>
2019-08-08 13:51:51 -0500

    make it better

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
