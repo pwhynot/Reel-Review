0654fbb13898a153cfa69825bb816557b71c43ed
unknown <pwhynotfilms@gmail.com>
2019-08-11 23:35:59 -0500

    style changes

--- client/src/components/layouts/jumbotron/index.js
+++ client/src/components/layouts/jumbotron/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Jumbotron, Container } from 'reactstrap';
+import './style.css';
 
 
 const jumbotron = (props) => {
+
   return (
     <div>
-      <Jumbotron fluid >
-
+      <Jumbotron fluid>
+        <div className="jumbotron jumbotron-cover-image">
         <Container fluid>
           <h1 className="display-3">Reel Review</h1>
           <p className="lead">Film and Television reviews from everyday people, not overpaid critics!</p>
-          
-        
-        </Container>
+          </Container>
+        </div>
+    
       </Jumbotron>
     </div>
   );
--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 //import Polls from '../../onlinePoll/Polls';
-import Card from '../../layouts/card/index';
+
 
  class Home extends Component {
     render() {
@@ -8,7 +8,7 @@ import Card from '../../layouts/card/index';
             <div>
                <h2>Welcome to Reel Review!</h2> 
                <h4>Check out the latest film and televison news below and be sure take our online poll of the week!</h4>
-        <Card />
+      
 
             </div>
         )
