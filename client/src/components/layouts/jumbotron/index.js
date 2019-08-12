e612e16b4e2c6ee5c5207ab70d4bb8a4277cd4c4
unknown <pwhynotfilms@gmail.com>
2019-08-11 23:48:33 -0500

    jumbotron image changed

--- client/src/components/layouts/jumbotron/index.js
+++ client/src/components/layouts/jumbotron/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Container } from 'reactstrap';
+import { Jumbotron } from 'reactstrap';
 import './style.css';
 
 
@@ -7,12 +7,11 @@ const jumbotron = (props) => {
 
   return (
     <div>
-      <Jumbotron fluid>
+      <Jumbotron>
         <div className="jumbotron jumbotron-cover-image">
-        <Container fluid>
-          <h1 className="display-3">Reel Review</h1>
+          <h1 className="display-2">Reel Review</h1>
           <p className="lead">Film and Television reviews from everyday people, not overpaid critics!</p>
-          </Container>
+          
         </div>
     
       </Jumbotron>
