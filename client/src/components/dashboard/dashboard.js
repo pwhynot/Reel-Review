751e0ef23aa3df90a3a8eda862a770a4b9e1ba2e
unknown <pwhynotfilms@gmail.com>
2019-08-13 01:54:26 -0500

    fixing errors

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
-
-
+//import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
 
 class Dashboard extends Component {
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+
 
   render() {
     const { user } = this.props.auth;
 
     return (
       <div>
+
         <div style={{ height: "25vh" }} className="container valign-wrapper">
           <div className="row">
             <div className="landing-copy col s12 left-align">
@@ -18,19 +26,39 @@ class Dashboard extends Component {
                   This is your profile, you can access all your reviews from here.
                 </p>
               </h4>
+              <p>
+              </p>
+              <button style={{width: "150px", borderRadius: "3px", letterSpacing: "1.5px"}} 
+                onClick={this.onLogoutClick}
+                className="btn btn-large btn-flat">
+                Logout
+              </button>
             </div>
           </div>
         </div>
-        <ul class="collection with-header" style={{width: "500px", marginLeft: "30px"}}>
+        <ul class="collection with-header" style={{width: "300px", marginLeft: "30px"}}>
         <li class="collection-header"><h4>Your Reivews</h4></li>
-        <li class="collection-item">Review 1 </li>
+        <li class="collection-item">Review 1</li>
         <li class="collection-item">Review 2</li>
         <li class="collection-item">Review 3</li>
         <li class="collection-item">Review 4</li>
       </ul>
+        
     </div>
     );
   }
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Dashboard);
